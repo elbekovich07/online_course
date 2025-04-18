86964e393861d1159f564142e52ee1bb37684560
normuminov <olmosnormuminov02@gmail.com>
2025-04-18 18:25:25 +0500

    'aa'

--- courses/forms.py
+++ courses/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Course, Teacher, Subject
+from django import forms
+from django.contrib.auth.models import User
 
 class CourseForm(forms.ModelForm):
     class Meta:
@@ -15,3 +17,48 @@ class TeacherForm(forms.ModelForm):
     class Meta:
         model = Teacher
         fields = '__all__'
+
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter your email'
+    }))
+    password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter your password'
+    }))
+
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter password'
+    }))
+    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Repeat password'
+    }))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter username'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter email'}),
+        }
+
+    def clean_password2(self):
+        password = self.cleaned_data.get("password")
+        password2 = self.cleaned_data.get("password2")
+        if password and password2 and password != password2:
+            raise forms.ValidationError("Passwords don't match!")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
--- courses/models.py
+++ courses/models.py
@@ -57,3 +57,12 @@ class Teacher(models.Model):
 
     def __str__(self):
         return self.full_name
+
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
